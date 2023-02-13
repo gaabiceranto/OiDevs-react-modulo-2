e6de3a329694e0751d06102a10daec2198c3b0b4
Gabriela Ceranto <gabiceranto@gmail.com>
2023-02-13 17:51:09 -0300

    aula-04-finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,63 @@ import "./index.css";
 import { Highlights } from './components/highlights/Highlights';
 import { useEffect } from 'react';
 import { fetcher } from './components/services';
+import React from 'react';
 
 
+function funcaoMemoizada(){
+  return "teste" 
+}
 
 function App() {
-  useEffect (() => {
-    const makeRequest = async () => {
-      const response = await fetcher ("photos");
-      console.log (response)
-    };
-    makeRequest();
-    
-  },[])
+  const [photos, setPhotos] = React.useState([])
+  const [text, setText] = React.useState();
 
+  // useEffect (() => {
+  //   const makeRequest = async () => {
+  //     const response = await fetcher ("photos");
+  //     setPhotos (response)
+  //   };
+  //   makeRequest();
+    
+  // },[]) 
+
+
+
+  // **** Use memo exemplo  (quando vc precisa executar uma ação quando algo do componete alterar))
+
+  const allPhotosMemoized = React.useMemo(()=>{
+    // console.log('calling memoized function')
+    return photos.map(item => item.urls.small)
+  }, [photos])
+
+  const allSmallPhotosNOTMemoized = () => {
+    // console.log('calling NOT memoized function')
+    return photos.map(item => item.urls.small)
+  }
+  
+  allSmallPhotosNOTMemoized();
+
+// **** Use memo exemplo  (quando vc precisa executar uma função complexa (side Effect))
+  const onChangeHandler = React.useCallback((e) =>{
+    console.log ('chamando set text')
+    console.log(photos)
+    setText(e.currentTarget.value)
+  }, [photos])
+
+  const onChangeHandlerNOTCallback = (e) =>{
+    console.log ('chamando set text')
+    setText(e.currentTarget.value)
+  }
+
+  const searchImages = React.useCallback(async () => {
+    const response = await fetcher ("photos");
+    setPhotos (response)
+  },[])
+    
+
+  
+
+ 
   return(
     <>
     <Grid >
@@ -26,8 +70,12 @@ function App() {
         <Navbar/>
       </GridItem>
       <GridItem>
+        <input placeholder='TESTE INPUT' onChange={onChangeHandler} />
         <Header/>
         <Highlights/>
+        <button onClick={() => searchImages() }> Buscar Imagens</button>
+        {photos.map (photo => 
+        <img key= {photo.id} src={photo.urls.small}/>)}
       </GridItem>
     </Grid>
     </>
